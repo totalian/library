227a0cb1cf24353857cb30c43d13bb6366f59d3d
Zeki Mirza <zeki.mirza@googlemail.com>
2020-10-27 23:50:05 +0000

    added the form to add a new book

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
+const newBook = document.querySelector('#newbook')
+const modal = document.querySelector('#modal')
+const modalClose = document.querySelector('#modalclose')
+const newBookForm = document.querySelector('#newbookform')
+
 let myLibrary = []
 
-function Book(){
-    // constructor
+function Book(title,author,pages,hasRead){
+    this.title = title,
+    this.author = author,
+    this.pages = pages
+    this.hasRead = hasRead
 }
 
 function addBookToLibrary(book){
     myLibrary.push(book)
+}
+
+newBook.addEventListener('click',() => modal.style.display = "block")
+modalClose.addEventListener('click', () => modal.style.display = "none")
+newBookForm.addEventListener('submit',e => {
+    // e.preventDefault()
+    createBook()
+})
+
+function createBook(){
+    let title = document.querySelector('#ftitle').value
+    let author = document.querySelector('#fauthor').value
+    let pages = document.querySelector('#fpages').value
+    let hasReadArray = Array.from(document.querySelectorAll('input[name="hasRead"]'))
+    hasRead = (hasReadArray.filter(input => input.checked == true)[0].value == "true" ? true : false)
+    let book = new Book(title,author,pages,hasRead)
+    addBookToLibrary(book)
 }
